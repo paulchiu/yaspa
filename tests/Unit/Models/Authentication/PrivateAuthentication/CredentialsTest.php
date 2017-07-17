9fa8c6572612de58e7304eab6d133602282270b5
Paul Chiu <paulchiu@gmail.com>
2017-07-17 18:25:05 +1000

    Added test for credentials model.

---
+++ tests/Unit/Models/Authentication/PrivateAuthentication/CredentialsTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Yaspa\Tests\Unit\Models\Authentication\PrivateAuthentication;
+
+use PHPUnit\Framework\TestCase;
+use Yaspa\Models\Authentication\PrivateAuthentication\Credentials;
+
+class CredentialsTest extends TestCase
+{
+    public function testCanRetrieveUnsetCredential()
+    {
+        $credentials = new Credentials();
+        $this->assertNull($credentials->getApiKey());
+    }
+
+    public function testCanRetreiveCredential()
+    {
+        $credentials = new Credentials();
+        $credentials
+            ->setApiKey('foo')
+            ->setPassword('bar');
+        $this->assertNotEmpty($credentials->getApiKey());
+        $this->assertNotEmpty($credentials->getPassword());
+    }
+}
