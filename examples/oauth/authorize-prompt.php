79aba81773821dadd46be44b3697fb7a42e115a9
Paul Chiu <paulchiu@gmail.com>
2017-07-23 12:21:53 +1000

    Moved examples to relevant folder.

--- examples/oauth/authorize-prompt.php
+++ examples/oauth/authorize-prompt.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/../../bootstrap.php';
+require_once __DIR__.'/../bootstrap.php';
 
 // Get test config and initialise test data
 $testConfig = new Yaspa\Tests\Utils\Config();
