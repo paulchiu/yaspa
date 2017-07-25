b4ec959cb3bf2f1699fb6a0e5a17a8dd0f73185d
Paul Chiu <paulchiu@gmail.com>
2017-07-26 02:37:59 +1000

    Created customer models.

---
+++ src/AdminApi/Customer/Models/Address.php
@@ -0,0 +1,334 @@
+<?php
+
+namespace Yaspa\AdminApi\Customer\Models;
+
+/**
+ * Class Address
+ *
+ * @package Yaspa\AdminApi\Customer\Models
+ * @see https://help.shopify.com/api/reference/customer#show
+ */
+class Address
+{
+    /** @var int $id */
+    protected $id;
+    /** @var string $firstName */
+    protected $firstName;
+    /** @var string $lastName */
+    protected $lastName;
+    /** @var string $company */
+    protected $company;
+    /** @var string $address1 */
+    protected $address1;
+    /** @var string $address2 */
+    protected $address2;
+    /** @var string $city */
+    protected $city;
+    /** @var string $province */
+    protected $province;
+    /** @var string $country */
+    protected $country;
+    /** @var int $zip */
+    protected $zip;
+    /** @var string $phone */
+    protected $phone;
+    /** @var string $name */
+    protected $name;
+    /** @var string $provinceCode */
+    protected $provinceCode;
+    /** @var string $countryCode */
+    protected $countryCode;
+    /** @var string $countryName */
+    protected $countryName;
+    /** @var bool $default */
+    protected $default;
+
+    /**
+     * @return int
+     */
+    public function getId():? int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Address
+     */
+    public function setId(int $id): Address
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName():? string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $firstName
+     * @return Address
+     */
+    public function setFirstName(?string $firstName): Address
+    {
+        $this->firstName = $firstName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName():? string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     * @return Address
+     */
+    public function setLastName(?string $lastName): Address
+    {
+        $this->lastName = $lastName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCompany():? string
+    {
+        return $this->company;
+    }
+
+    /**
+     * @param string $company
+     * @return Address
+     */
+    public function setCompany(?string $company): Address
+    {
+        $this->company = $company;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddress1():? string
+    {
+        return $this->address1;
+    }
+
+    /**
+     * @param string $address1
+     * @return Address
+     */
+    public function setAddress1(string $address1): Address
+    {
+        $this->address1 = $address1;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddress2():? string
+    {
+        return $this->address2;
+    }
+
+    /**
+     * @param string $address2
+     * @return Address
+     */
+    public function setAddress2(?string $address2): Address
+    {
+        $this->address2 = $address2;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity():? string
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param string $city
+     * @return Address
+     */
+    public function setCity(string $city): Address
+    {
+        $this->city = $city;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getProvince():? string
+    {
+        return $this->province;
+    }
+
+    /**
+     * @param string $province
+     * @return Address
+     */
+    public function setProvince(string $province): Address
+    {
+        $this->province = $province;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountry():? string
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param string $country
+     * @return Address
+     */
+    public function setCountry(string $country): Address
+    {
+        $this->country = $country;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getZip():? int
+    {
+        return $this->zip;
+    }
+
+    /**
+     * @param int $zip
+     * @return Address
+     */
+    public function setZip(int $zip): Address
+    {
+        $this->zip = $zip;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhone():? string
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param string $phone
+     * @return Address
+     */
+    public function setPhone(?string $phone): Address
+    {
+        $this->phone = $phone;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName():? string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Address
+     */
+    public function setName(string $name): Address
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getProvinceCode():? string
+    {
+        return $this->provinceCode;
+    }
+
+    /**
+     * @param string $provinceCode
+     * @return Address
+     */
+    public function setProvinceCode(string $provinceCode): Address
+    {
+        $this->provinceCode = $provinceCode;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountryCode():? string
+    {
+        return $this->countryCode;
+    }
+
+    /**
+     * @param string $countryCode
+     * @return Address
+     */
+    public function setCountryCode(string $countryCode): Address
+    {
+        $this->countryCode = $countryCode;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountryName():? string
+    {
+        return $this->countryName;
+    }
+
+    /**
+     * @param string $countryName
+     * @return Address
+     */
+    public function setCountryName(string $countryName): Address
+    {
+        $this->countryName = $countryName;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isDefault():? bool
+    {
+        return $this->default;
+    }
+
+    /**
+     * @param bool $default
+     * @return Address
+     */
+    public function setDefault(bool $default): Address
+    {
+        $this->default = $default;
+        return $this;
+    }
+
+}
---
+++ src/AdminApi/Customer/Models/Customer.php
@@ -0,0 +1,415 @@
+<?php
+
+namespace Yaspa\AdminApi\Customer\Models;
+
+use DateTime;
+
+/**
+ * Class Customer
+ *
+ * @package Yaspa\AdminApi\Customer\Models
+ * @see https://help.shopify.com/api/reference/customer#show
+ */
+class Customer
+{
+    /** @var int $id */
+    protected $id;
+    /** @var string $email */
+    protected $email;
+    /** @var bool $acceptsMarketing */
+    protected $acceptsMarketing;
+    /** @var DateTime $createdAt */
+    protected $createdAt;
+    /** @var DateTime $updatedAt */
+    protected $updatedAt;
+    /** @var string $firstName */
+    protected $firstName;
+    /** @var string $lastName */
+    protected $lastName;
+    /** @var int $ordersCount */
+    protected $ordersCount;
+    /** @var string $state */
+    protected $state;
+    /** @var string $totalSpent */
+    protected $totalSpent;
+    /** @var int $lastOrderId */
+    protected $lastOrderId;
+    /** @var string $note */
+    protected $note;
+    /** @var bool $verifiedEmail */
+    protected $verifiedEmail;
+    /** @var string $multipassIdentifier */
+    protected $multipassIdentifier;
+    /** @var bool $taxExempt */
+    protected $taxExempt;
+    /** @var string $phone */
+    protected $phone;
+    /** @var string $tags */
+    protected $tags;
+    /** @var string $lastOrderName */
+    protected $lastOrderName;
+    /** @var array|Address[] $addresses */
+    protected $addresses;
+    /** @var Address $defaultAddress */
+    protected $defaultAddress;
+
+    /**
+     * @return int
+     */
+    public function getId():? int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Customer
+     */
+    public function setId(int $id): Customer
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail():? string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return Customer
+     */
+    public function setEmail(string $email): Customer
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAcceptsMarketing():? bool
+    {
+        return $this->acceptsMarketing;
+    }
+
+    /**
+     * @param bool $acceptsMarketing
+     * @return Customer
+     */
+    public function setAcceptsMarketing(bool $acceptsMarketing): Customer
+    {
+        $this->acceptsMarketing = $acceptsMarketing;
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getCreatedAt():? DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param DateTime $createdAt
+     * @return Customer
+     */
+    public function setCreatedAt(DateTime $createdAt): Customer
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getUpdatedAt():? DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param DateTime $updatedAt
+     * @return Customer
+     */
+    public function setUpdatedAt(DateTime $updatedAt): Customer
+    {
+        $this->updatedAt = $updatedAt;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName():? string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $firstName
+     * @return Customer
+     */
+    public function setFirstName(string $firstName): Customer
+    {
+        $this->firstName = $firstName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName():? string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     * @return Customer
+     */
+    public function setLastName(string $lastName): Customer
+    {
+        $this->lastName = $lastName;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOrdersCount():? int
+    {
+        return $this->ordersCount;
+    }
+
+    /**
+     * @param int $ordersCount
+     * @return Customer
+     */
+    public function setOrdersCount(int $ordersCount): Customer
+    {
+        $this->ordersCount = $ordersCount;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getState():? string
+    {
+        return $this->state;
+    }
+
+    /**
+     * @param string $state
+     * @return Customer
+     */
+    public function setState(?string $state): Customer
+    {
+        $this->state = $state;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTotalSpent():? string
+    {
+        return $this->totalSpent;
+    }
+
+    /**
+     * @param string $totalSpent
+     * @return Customer
+     */
+    public function setTotalSpent(string $totalSpent): Customer
+    {
+        $this->totalSpent = $totalSpent;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLastOrderId():? int
+    {
+        return $this->lastOrderId;
+    }
+
+    /**
+     * @param int $lastOrderId
+     * @return Customer
+     */
+    public function setLastOrderId(?int $lastOrderId): Customer
+    {
+        $this->lastOrderId = $lastOrderId;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNote():? string
+    {
+        return $this->note;
+    }
+
+    /**
+     * @param string $note
+     * @return Customer
+     */
+    public function setNote(?string $note): Customer
+    {
+        $this->note = $note;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isVerifiedEmail():? bool
+    {
+        return $this->verifiedEmail;
+    }
+
+    /**
+     * @param bool $verifiedEmail
+     * @return Customer
+     */
+    public function setVerifiedEmail(bool $verifiedEmail): Customer
+    {
+        $this->verifiedEmail = $verifiedEmail;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMultipassIdentifier():? string
+    {
+        return $this->multipassIdentifier;
+    }
+
+    /**
+     * @param string $multipassIdentifier
+     * @return Customer
+     */
+    public function setMultipassIdentifier(?string $multipassIdentifier): Customer
+    {
+        $this->multipassIdentifier = $multipassIdentifier;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isTaxExempt():? bool
+    {
+        return $this->taxExempt;
+    }
+
+    /**
+     * @param bool $taxExempt
+     * @return Customer
+     */
+    public function setTaxExempt(bool $taxExempt): Customer
+    {
+        $this->taxExempt = $taxExempt;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhone():? string
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param string $phone
+     * @return Customer
+     */
+    public function setPhone(?string $phone): Customer
+    {
+        $this->phone = $phone;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTags():? string
+    {
+        return $this->tags;
+    }
+
+    /**
+     * @param string $tags
+     * @return Customer
+     */
+    public function setTags(string $tags): Customer
+    {
+        $this->tags = $tags;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastOrderName():? string
+    {
+        return $this->lastOrderName;
+    }
+
+    /**
+     * @param string $lastOrderName
+     * @return Customer
+     */
+    public function setLastOrderName(?string $lastOrderName): Customer
+    {
+        $this->lastOrderName = $lastOrderName;
+        return $this;
+    }
+
+    /**
+     * @return array|Address[]
+     */
+    public function getAddresses():? array
+    {
+        return $this->addresses;
+    }
+
+    /**
+     * @param array|Address[] $addresses
+     * @return Customer
+     */
+    public function setAddresses(array $addresses): Customer
+    {
+        $this->addresses = $addresses;
+        return $this;
+    }
+
+    /**
+     * @return Address
+     */
+    public function getDefaultAddress():? Address
+    {
+        return $this->defaultAddress;
+    }
+
+    /**
+     * @param Address $defaultAddress
+     * @return Customer
+     */
+    public function setDefaultAddress(Address $defaultAddress): Customer
+    {
+        $this->defaultAddress = $defaultAddress;
+        return $this;
+    }
+}
